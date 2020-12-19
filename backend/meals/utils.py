f61c8134243bc52a9c7398686a5ca72a54e2fa0d
chivke <chivke@pm.me>
2020-12-18 22:43:20 -0300

    documents meals.utils module

--- backend/meals/utils.py
+++ backend/meals/utils.py
@@ -9,6 +9,9 @@ from celery.task.schedules import crontab
 
 def cron_meals_preference_limit():
     '''
+    It translates the cutoff time for selecting lunches set
+    in variable MEALS_PREFERENCE_LIMIT and returns a crontab
+    schema instance for celery.
     '''
     limit = dt.datetime.strptime(settings.MEALS_PREFERENCE_LIMIT, '%H:%M')
     return crontab(hour=limit.hour, minute=limit.minute)
@@ -16,6 +19,8 @@ def cron_meals_preference_limit():
 
 def get_slack_reminder(menu, employee):
     '''
+    Function that generates the message that is sent through slack
+    along with the menu of the day and the dishes available.
     '''
     message = f'Hello {employee.username}!\n'\
               'I share with you today\'s menu :)\n'
