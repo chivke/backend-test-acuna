93543655a5d26bb015daefbd659bef5151d83b31
chivke <chivke@pm.me>
2020-12-18 22:45:15 -0300

    documents meals.views.common module

--- backend/meals/views/common.py
+++ backend/meals/views/common.py
@@ -9,6 +9,10 @@ from backend.meals.models import MenuModel
 
 
 class RootRouterView(LoginRequiredMixin, RedirectView):
+    '''
+    View that controls the root of the application and redirects in
+    relation to whether the user is authenticated, is an employee or is Nora.
+    '''
 
     def get_redirect_url(self, *args, **kwargs):
         if self.request.user.profile.is_nora:
